6fa3e810414cd2f9c0034745f6eb7ce5afb1d487
Maipham72 <hongmai.pham@student.adelaide.edu.au>
2023-03-08 11:30:35 +1030

    update

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -2,8 +2,8 @@
 
 int sum_diagonal(int array[4][4]) {
   int sum = 0;
-  for (int row = 0; row < 4; row++) {
-    for (int col = 0; col < 4; col++) {
+  for (int row = 0; row <= 4; row++) {
+    for (int col = 0; col <= 4; col++) {
       if (row = col) {
         sum = sum + array[row][col];
       }
--- main-2-1.cpp
+++ main-2-1.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <string>
 
 extern void print_binary_str (std::string);
 int main() {
-  char n;
+  std::string s;
 
   std::cout << "Enter a number: ";
-  std::cin >> n;
+  std::cin >> s;
 
-  print_binary_str(n);
-  
+  print_binary_str(s);
+
   return 0;
 }
