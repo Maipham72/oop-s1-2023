5448d7d0505c608cef42be1522d998d621fe60f1
Maipham72 <hongmai.pham@student.adelaide.edu.au>
2023-03-08 11:51:40 +1030

    fix bugs

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -2,9 +2,9 @@
 
 int sum_diagonal(int array[4][4]) {
   int sum = 0;
-  for (int row = 0; row <= 4; row++) {
-    for (int col = 0; col <= 4; col++) {
-      if (row = col) {
+  for (int row = 0; row < 4; row++) {
+    for (int col = 0; col < 4; col++) {
+      if (row == col) {
         sum = sum + array[row][col];
       }
     }
