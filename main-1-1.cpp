6fe639e402c4b39aafd6cbda459c78842eefd807
Maipham72 <hongmai.pham@student.adelaide.edu.au>
2023-03-14 17:14:35 +1030

    adding ws03

---
+++ main-1-1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+extern int size_of_variable_star_t();
+int main() {
+  size_of_variable_star_t;
+  return 0;
+}
---
+++ main-2-4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+extern float add_op(float left, float right);
+extern float subtract_op(float left, float right);
+extern float multiply_op(float left, float right);
+int main() {
+  float a;
+  float b;
+
+  cout << "Enter a number: "; 
+  cin >> a;
+  cout << "Enter another number: ";
+  cin >> b;
+
+  add_op(a,b);
+  subtract_op(a,b);
+  multiply_op(a,b);
+
+  return 0;
+}
